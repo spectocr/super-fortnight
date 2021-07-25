95b70a5eb55a8ddd10e4299e3e555d0fb2faceb8
spectocr <highstakes22@gmail.com>
2021-07-25 12:40:35 -0400

    pushing bones

---
+++ public/javascript/login.js
@@ -0,0 +1,68 @@
+const loginForm = document.querySelector('.login-form');
+
+
+
+
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+
+  console.log(loginForm);
+  const email = loginForm.querySelector('input[name="uname"]').value;
+  console.log(email);
+  //const password = loginForm.querySelector('[name="pwd"]').value;
+  
+  //console.log(password);
+  
+
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+/*
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+*/
+//loginForm.addEventListener('submit', loginFormHandler);
+
+/*
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+*/
