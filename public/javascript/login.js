441c4c4b38bd826bb55b1cf2c85b9dc5f6a159c2
mlopez <lopezmatthew87@gmail.com>
2021-07-25 14:22:47 -0400

    testing out login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -63,6 +63,15 @@ async function signupFormHandler(event) {
 }
 */
 
+document.getElementById('display').addEventListener('click',
+function() {
+  event.preventDefault();
+
+    alert("Info is here");
+});
+
+
+
 // open modal
 document.getElementById('regNow').addEventListener('click',
 function() {
@@ -79,15 +88,6 @@ function() {
 
 
 
-document.getElementById('passNow').addEventListener('click',
-function() {
-    document.querySelector('.passReset').style.display = 'flex';
-});
-
-document.getElementsByClassName('close2').addEventListener('click', 
-function() {
-    document.querySelector('.passReset').style.display = 'none';
-});
 
 loginForm.addEventListener('submit', loginFormHandler);
 
